424e83f91c40f588585f30ab0d97685c39c41bae
Kasandra Darwin <kasandradarwin@gmail.com>
2022-09-16 03:26:33 +0000

    added top border

--- boxit.js
+++ boxit.js
@@ -1,7 +1,14 @@
 // In this homework, you will create a script that can take any number of arguments then outputs them inside boxes.
 
 function drawLine(num1){
-    console.log
+    console.log("━".repeat(num1))
 }
 
-drawLine(4)
+//drawLine(12)
+
+
+function drawTopBorder(num1){
+    console.log("┏" + "━".repeat(num1-2) + "┓")
+}
+
+drawTopBorder(12)
