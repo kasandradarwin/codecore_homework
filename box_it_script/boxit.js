480db6a20526c48fb956309f379989cfbeca7bad
Kasandra Darwin <kasandradarwin@gmail.com>
2022-09-17 22:34:47 +0000

    making sure everything is up to date

--- box_it_script/boxit.js
+++ box_it_script/boxit.js
@@ -1,10 +1,12 @@
 // In this homework, you will create a script that can take any number of arguments then outputs them inside boxes.
 
+//draws a line of the requested length
+
 function drawLine(num1){
     return("━".repeat(num1))
 }
 
-
+// draws the top border, length determines by number passed in
 function drawTopBorder(num1){
     if (num1 < 1){
         console.log("┏" + "┓")
@@ -15,6 +17,7 @@ function drawTopBorder(num1){
 }
 }
 
+// draws the middle border, length determines by number passed in
 function drawMiddleBorder(num1) {
     if (num1 < 1){
         console.log("┣" + "┫")
@@ -25,6 +28,7 @@ function drawMiddleBorder(num1) {
     }
 }
 
+// draws the bottom border, length determines by number passed in
 function drawBottomBorder(num1) {
     if (num1 < 1){
         console.log("┗" + "┛")
@@ -35,23 +39,27 @@ function drawBottomBorder(num1) {
     }
 }
 
+//draws bars around the string passed in
+//since the boxIt function takes in an array-- this function adds padding to any strings shorter than the longest string
+//this way all the boxes are the same size
 function drawBarsAround(theString, longestLength) {
     if (longestLength > theString.length){
         let extraBoxSize = parseInt(longestLength-theString.length)
-    
         let padding = " ".repeat(extraBoxSize)
         console.log("┃" + theString + padding + "┃")
     } else {
         console.log("┃" + theString + "┃")
     }
 }
+//boxes the strings passed in using the above functions
 
 function boxIt(arr){  
-   let longestLength = Math.max(...(arr.map(columnWidth => columnWidth.length)));
+    // grabs the length of the longest string to determine the box size
+   const longestLength = Math.max(...(arr.map(columnWidth => columnWidth.length)));
         
      console.log(`${drawTopBorder(longestLength)}`)
 
-    // so the middle bar isn't repeated at the very end
+    // to fill the inside of the box, and make sure the middle bar isn't repeated at the very end
         for (let j = 0; j < arr.length; j++) {
             drawBarsAround(arr[j], longestLength)
             if (j == arr.length-1){
