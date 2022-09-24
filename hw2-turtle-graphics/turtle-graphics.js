000eab02530a43d961d3f7439f6a13265bc86b7c
Kasandra Darwin <kasandradarwin@gmail.com>
2022-09-24 02:35:31 +0000

    added the turtle class, and first method, forward

---
+++ hw2-turtle-graphics/turtle-graphics.js
@@ -0,0 +1,14 @@
+class Turtle{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+new Turtle (0,0)
+
+Turtle.prototype.forward = function(forward) {
+    return new Vector(
+       this.forward = forward;
+    
+   }
