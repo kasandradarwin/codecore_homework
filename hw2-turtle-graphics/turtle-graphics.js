27cf2cf669b500a3e53e70100ac1870ad8ff1545
Kasandra Darwin <kasandradarwin@gmail.com>
2022-09-25 01:09:42 +0000

    messing around with prototype formatting, I keep getting an unexpected ___ error

--- hw2-turtle-graphics/turtle-graphics.js
+++ hw2-turtle-graphics/turtle-graphics.js
@@ -1,48 +1,85 @@
+//Codecore Week 2 homework
+
 
 // To begin drawing, your program needs to know where it should begin. 
 //Create a Turtle class whose constructor will take two arguments (in order): x & y coordinates. 
+const camino = [];
 
-class Turtle{
+class Turtle {
     constructor(x,y){
         this.x = x || 0;
         this.y = y || 0;
+        camino.push = [[this.x, this.y]]
+        this.direction = "east"
 
         //Create a forward method that takes a number of steps 
         //then updates the Turtle instance with its new position after moving that many steps.
          //Keep track of every movement the turtle makes including the first one.
-        this.forward = function (steps) {
+    }
+         //X axis = West <> East
+         //Y axis = North <> South
+        Turtle.prototype = {
+            forward : function(steps){
+            
             this.steps = steps;
             this.x += steps;
             this.y += steps
+                if(direction == "north") {
+                    this.y = this.y-steps
+                } else if (direction == "east") {
+                    this.x = this.x + steps
+                } else if (direction == "south") {
+                    this.y = this.y + steps
+                } else if (direction == "west") {
+                    this.x = this.x - steps
+                }
+            camino.push([this.x, this.y])   
+            } 
+            //right : function() {}
         }
+        };
+
+    const flash = new Turtle(0, 0)
+    console.log(flash.forward(5).forward(3).forward(2).print())
+
+    //console.log(flash.forward(5).right().forward(5).right().forward(5).right().forward(5).print())
+    console.log(camino)
+
+
         //Create a right method that takes zero arguments.
         // When right is called, update the Turtle instance to rotate its facing to the right. 
         // A turtle should begin facing east.
-        this.right = function () {
-            this.direction = east;
-        }
+        // this.right = function () {
+        //     this.direction = east;
+        //     camino.push()
+        // }
 
         // Create a left method like right but turns the turtle's facing to the left.
-        this.right = function () {
-            this.direction = west;
-        }
+        // this.left = function () {
+        //     this.direction = west;
+        //     camino.push()
+//         // }
 
-        this.allPoints()
+//         this.allPoints = function (){
+//             console.log(this.camino)
+//         }
+
+//         print(){
+//             console.log("--BEGIN LOG")
+
+//             console.log("--END LOG")
+//         }
     
-    }
-
-   
-    allPoints()
-    print()
-}
+//     }
+// }
 
 
-new Turtle(0, 0)
-    .forward(5)
-    .right()
-    .forward(5)
-    .right()
-    .forward(5)
-    .right()
-    .forward(5)
-    .print()
+// const flash = new Turtle(0, 0)
+//     flash.forward(5)
+//     .right()
+//     .forward(5)
+//     .right()
+//     .forward(5)
+//     .right()
+//     .forward(5)
+//     .print()
