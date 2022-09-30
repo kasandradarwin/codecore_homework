a61b9d7aaebeaeefbe4c49553c0a5b2425c04ea2
Kasandra Darwin <kasandradarwin@gmail.com>
2022-09-30 17:31:53 +0000

    removed some unnecessary codes and comments, added additional notes

--- hw2-turtle-graphics/turtle-graphics.js
+++ hw2-turtle-graphics/turtle-graphics.js
@@ -8,19 +8,18 @@ class Turtle {
         this.allpoints.push([x,y])
         this.direction = "east"
 
-        //setting the initial values, will be changed and use to set the grid size later.
-        this.maxX = this.x;
+        this.maxX = this.x; //these will be used to create the grid later. Values will be updated in the forward() function
         this.maxY = this.y;
  
     }
 
          //X axis = West <> East
          //Y axis = North <> South
-         forward(steps) {
+        forward(steps) {
             for(let z = 0; z <steps; z++) {
-
-            //checks to see what the currect direction is, and moved the turtle forward or backward in the right direction
+            //checks to see what the currect direction is, and moves the turtle forward or backward in thatdirection
             //iterates through one by one and pushes to allpoints array each time to track every step 
+
                 if (this.direction === "north") {
                     this.y--
                     this.allpoints.push([this.x,this.y])
@@ -37,10 +36,9 @@ class Turtle {
                     this.x--
                     this.allpoints.push([this.x,this.y])
                 }
-
             }   
 
-            //getting the highest value for the x axis and y axis, will be used to create the grid later
+        //getting the highest value for the x axis and y axis, will be used to create the grid later
 
             if(this.x > this.maxX){
                 this.maxX = this.x 
@@ -48,103 +46,75 @@ class Turtle {
                 this.maxY = this.y
             }
 
-            if(this.x < this.minX){
-                this.minX = this.x 
-            }  else if (this.y > this.minY){
-                this.minY = this.y
+        return this;
+        } 
+
+        right() {
+        //checks the current direction to turn the turn "right" from the current direction
+            if (this.direction === "north") {
+                this.direction = "east"
+
+            } else if (this.direction == "east") {
+                this.direction = "south"
+
+            } else if (this.direction === "south") {
+                this.direction = "west"
+
+            } else if (this.direction === "west") {
+                this.direction = "north"
             }
-
-            //getting the lowest value for the x axis and y axis, will be used to create the grid later
-
-            return this;
-            } 
-
-            right() {
-            //checks the current direction to turn the turn "right" from the current direction
-                if (this.direction === "north") {
-                    this.direction = "east"
-
-                } else if (this.direction == "east") {
-                    this.direction = "south"
-
-                } else if (this.direction === "south") {
-                    this.direction = "west"
-
-                } else if (this.direction === "west") {
-                    this.direction = "north"
-                }
-                return this;
-            }
+        return this;
+        }
         
 
-            left() {
-            //checks the current direction, turns the turtle left
+        left() {
+        //checks the current direction, turns the turtle left
 
-                if (this.direction == "north") {
-                    this.direction = "west"
+            if (this.direction == "north") {
+                this.direction = "west"
 
-                } else if (this.direction == "east") {
-                    this.direction = "north"
+            } else if (this.direction == "east") {
+                this.direction = "north"
 
-                } else if (this.direction == "south") {
-                    this.direction = "east"
+            } else if (this.direction == "south") {
+                this.direction = "east"
 
-                } else if (this.direction == "west") {
-                    this.direction = "south"
-                }
-                return this;
+            } else if (this.direction == "west") {
+                this.direction = "south"
             }
+            return this;
+        }
 
-            allPoints(){
-            return(this.allpoints)
+        allPoints(){
+        return(this.allpoints)
         } 
         
-            print(){
-                
-                // let startingPointX = this.allpoints[0][1];
-                // let startingPointY = this.allpoints[0][1];
-                // let finalPointX = this.allpoints[this.allpoints.length-1][0]
-                // let finalPointY = this.allpoints[this.allpoints.length-1][1]
-                // let xstart = this.minX;
-  
-                console.log(this.maxX, this.maxY)
-                console.log(this.allpoints)
+        print(){
+            const final = [];
+            const slicedpoints = this.allpoints;
 
-                    const final = [];
-                    const slicedpoints = this.allpoints;
-                    
-                    // if (startingPointX < finalPointX){
-                    //     this.maxX = this.maxX + startingPointX
-                    //     console.log(this.maxX, this.maxY)
-                      
-                    // }
-                    // if (startingPointY < finalPointY){
-                    //     this.maxY = this.maxY + startingPointY
-                    //     console.log(this.maxX, this.maxY)
-                      
-                    // }
-                    for (let i=0 ; i<this.maxX+2; i++){                    
-                        const x = []
-                        for(let j=0; j<this.maxY+2; j++){
-                          const gridPath = slicedpoints.find((item)=>{
-                            return item.join(",")===`${j},${i}` 
-                          })
-                          if(gridPath) x.push("■")
-                          else x.push("□")
-                        }
-                      
-                        final.push(x.join(""))
-                      }
-                      
-                      console.log("--BEGIN LOG")
-                      console.log(final.join("\n"))
-                      console.log("--END LOG")
-            }
+            for (let i=0 ; i<this.maxX+2; i++){         //iterates through the widtn of the grid            
+                const x = []
+                for(let j=0; j<this.maxY+2; j++){  //iterates through the length of the grid
+                    const gridPath = slicedpoints.find((item)=>{
+                    return item.join(",")===`${j},${i}` 
+                    })
+                    if(gridPath) x.push("■") // if the box is crossed by the turtle-- fills the box, pushes to x array
+                    else x.push("□") // if not, the box will be empty- push to x array
+                }
+        
+                final.push(x.join("")) //pushes x arrays to final array
+                }
+                //joins and prints the final arrays-- printing them on their own lines to print the grid.
+                console.log("--BEGIN LOG")
+                console.log(final.join("\n"))
+                console.log("--END LOG")
+    }
 
  }
 
         
- new Turtle(0, 4)
+ const Juanita = new Turtle(0, 4)
     .forward(3)
     .left()
     .forward(3)
@@ -159,3 +129,16 @@ class Turtle {
     .left()
     .forward(3)
     .print();
+
+const raphael = new Turtle(0, 0)
+.forward(5)
+.right()
+.forward(5)
+.right()
+.forward(5)
+.right()
+.forward(5)
+.print()
+
+const flash = new Turtle(0, 4).forward(3).left().forward(3).print();
+
