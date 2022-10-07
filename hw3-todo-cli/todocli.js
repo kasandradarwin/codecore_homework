2f972c61a8de70694edcad9c3c09a268e509930e
Kasandra Darwin <kasandradarwin@gmail.com>
2022-10-07 01:54:39 +0000

    adding some more comments, removed old commented code, moved the quit function into the switch statement

--- hw3-todo-cli/todocli.js
+++ hw3-todo-cli/todocli.js
@@ -1,8 +1,9 @@
 const readline = require("readline");
 const fs = require("fs");
 const { resourceLimits } = require("worker_threads");
-const tasks = [] //[['[ ]','make breakfast'],['[ ]','make breakfast'],['[ ]','make breakfast'],['[ ]','make breakfast']];
+const tasks = [];
 
+// creating the interface
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -10,17 +11,21 @@ const rl = readline.createInterface({
     prompt: "\n >" 
     })
 
+// the initial welcome message
+
 function welcome(){
     console.log("Welcome to Todo CLI \n \n  -------------------- \n \n")
     commandList()
-    //console.log(commandList())
-    //rl.prompt()
 }
+
+//the command list, I've written a function to call it, rather than retyping it each time.
 
 function commandList(){
-    console.log(" \n (v) View * (n) New * (cX) Complete * (dX) Delete * (q) Quit \n \n")
+    console.log(" \n (v) View * (n) New * (cX) Complete * (dX) Delete * (s) Save * (q) Quit \n \n")
 }
 
+
+// a switch statement/function to triage the command the user type, and call the appropriate function
 
 rl.on('line', (command) => {
     switch (command[0]){
@@ -42,7 +47,12 @@ rl.on('line', (command) => {
             break;
         case 'q':
             console.log("\n \n 👉 Quit 👈\n \n ")
-            quit() 
+            console.log("\n \n Finished already? Great work! See you soon😃\n \n")
+            rl.close()
+            break;
+        case 's':
+            console.log("\n \n 👉 Save List 👈\n \n ")
+            saveToDo()
             break;
         default:
             console.log("Hmm... that didn't work. Did ya wanna try that again? \n \n(v) View * (n) New * (cX) Complete * (dX) Delete * (q) Quit")
@@ -50,26 +60,23 @@ rl.on('line', (command) => {
     }
 });
 
-    
+    //displays the list in a nice, easy to read format. 
+    // Delays a half a sec from the command being received 
 function viewItem(){
     if (tasks.length > 0){
         setTimeout(function() {
         console.log("📝 To Do List\n")
         for (let i = 0; i < tasks.length; i++) {
-            console.log(i, tasks[i].join(" ")) 
-            //console.log(i, tasks[i])   
+            console.log(i, tasks[i].join(" "))  
         }
         commandList()
         }, 500)
         } else {
         console.log("Your To Do list is empty, you can add an item but pressing 'n'")
-
     }
+}
 
-    //From the Todo Menu, pressing v then Enter should display the contents of the todo
-    // list then the Todo Menu again. 
-    //When displaying the list, completed items should have a checkmark (i.e. `[✓]`) besides their title.
-    }
+// adds a new item to the list, pushing it to the array that is processed in viewItem()
 
 function newItem(){
     rl.question("What new item would you like to add to the list? \n", (newitem) => {
@@ -83,33 +90,35 @@ function newItem(){
         })  
     }
 
-//From the Todo Menu pressing n then Enter should ask the user what item to add to the list. 
-//The user can then write a response. Save their response as a new item at the end of the todo list.
+// marks an item on the list as complete by the index. Error logging for
+// if list is empty, and for if the index is out of range or can't be interpreted.
+// each provides a helpful suggestion
    
 function completeItems(checkOff){
     if (tasks.length == 0){
-        console.log("Your To Do list is empty, you can add an item but pressing 'n'")
+        console.log("Whoops! Your To Do list is empty, you can add an item but pressing 'n'")
     } else {
         try{
-            //setTimeout(function() {
             console.log(`Marking: '${tasks[checkOff][1]}' complete...\n`)
             tasks[checkOff][0] = "[✓]"
-           // }, 1000);
 
-            //setting the timeout just because I think it looks a bit more polished
+            //setting the timeout just because I think it looks nice 
 
             setTimeout(function() {
                 console.log("✅ Success! Here is your updated list: \n \n")
                 viewItem()
-            }, 1000);
+            }, 500);
         } catch(err) {
-            console.log(`🥹 Whoops, that index doesn't correspond with an item in the list-- try one of these!`)
-            console.table(tasks)
+            console.log(`🥹 Whoops, that index doesn't correspond with an item in the list-- try one of these! \n \n`)
+            viewItem()
+        }   
     }
+
 }
-
-    }
-
+// same as completeItems(), minus the obvious
+// deletes an item from the list by the index. Error logging for
+// if list is empty, and for if the index is out of range or can't be interpreted.
+// each provides a helpful suggestion
 
 function deleteItem(toDelete){
     if (tasks.length == 0){
@@ -125,46 +134,65 @@ function deleteItem(toDelete){
             }, 500);
         
         } catch(err) {
-            console.log(`🥹 Whoops, that index doesn't correspond with an item in the list-- try one of these!`)
-            console.table(tasks)        
+            console.log(`🥹 Whoops, that index doesn't correspond with an item in the list-- try one of these! \n \n`)
+            viewItem()      
         }
     }
 }
-function quit(){
-    //From the Todo Menu pressing q quits the application. Say farewell.
-    console.log("Finished already? Great work! See you soon😃")
-    rl.close()
+   
+// You can save your to do list to a file    
+function saveToDo() {
+    rl.question("What would you like to name your file? Press enter to use 'toDoList.json' \n", (userfilename) => {
+        let jsonobj = [];
 
-    }
+        filename = userfilename || 'toDoList.json';
 
-    if (process.argv[2]) {
-        fs.readFile(process.argv[2], {encoding: 'utf-8'}, (err, data) => {
-          if (err) throw err;
-          for (let i = 0; i < JSON.parse(data).length; i++) {
-            parsed = (JSON.parse(data)[i])
+        for (let item of tasks) {
+            if (item.includes('[✓]')){
+                jsonobj.push({"completed": true, "title": (item.slice(1)).join(" ")})
+            } else {
+                jsonobj.push({"completed": false, "title": (item.slice(1)).join(" ")})
+            }
             
-                if (parsed.completed=== false){
-                    tasks.push(["[ ]",parsed.title])
-                } else if (parsed.completed===true){
-                    tasks.push(["[✓]",parsed.title])
-                }
-            //console.log(tasks)
-            
-            // console.log(parsed.completed)
-            // console.log(parsed.title)
-            
-            //tasks.push(JSON.parse(data)[i]);
-            
-          } //console.log(tasks)
+        }
+
+    const jsonString = JSON.stringify(jsonobj)
+     
+        fs.writeFile(filename, jsonString, err =>{
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("List saved to ", filename)
+            }
         })
-      }
 
+        setTimeout(function() {
+            console.log(`\n \n ✅${filename} has been successfully saved\n \n`)
+        commandList()
+        }   , 500);
+        })  
+
+}
+
+// reads a file passed in as an argument, parses the info 
+// and adds it to the list seamlessly LIKE NOTHING HAPPENED
+
+if (process.argv[2]) {
+    fs.readFile(process.argv[2], {encoding: 'utf-8'}, (err, data) => {
+      if (err) throw err;
+      for (let i = 0; i < JSON.parse(data).length; i++) {
+        parsed = (JSON.parse(data)[i])
+            if (parsed.completed=== false){
+                tasks.push(["[ ]",parsed.title])
+            } else if (parsed.completed===true){
+                tasks.push(["[✓]",parsed.title])
+            }
+        } 
+    })
+  }
             
         
 
 welcome()
 
 
-
-
-// stretch: https://nodejs.org/docs/latest-v15.x/api/readline.html#readline_example_read_file_stream_line_by_line
